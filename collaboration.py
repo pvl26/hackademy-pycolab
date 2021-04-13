216711f3b3ab6de86c54e8020b09ebb5140d22c7
nico-corneanu <72967271+nico-corneanu@users.noreply.github.com>
2021-04-13 20:08:55 +0300

    written greet (#43)
    Co-authored-by: Nicoleta Corneanu <nicocorneanu23@gmail.com>

--- collaboration.py
+++ collaboration.py
@@ -125,7 +125,7 @@ def greet_miruna_elena_banu():
 	return 'Miruna-Elena Banu'
 
 def greet_nicoleta_corneanu():
-	return 'missing'
+	return 'Nicoleta Corneanu'
 
 def greet_nicu_chirila():
 	return 'missing'

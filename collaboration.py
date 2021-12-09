a207a4cd60ef3c75c43b75de037f0f6097a0eab9
Mihai Para <73332299+mpara30@users.noreply.github.com>
2021-12-09 20:45:13 +0200

    Mpara30/demo (#79)
    * First commit
    
    * First commit
    
    * Delete .gitignore
    
    * Removed .idea stuff

--- collaboration.py
+++ collaboration.py
@@ -194,7 +194,7 @@ def greet_tarik_ilhan_omer():
 	return 'Tarik Ilhan Omer'
 
 def greet_mihai_para():
-	return 'missing'
+	return 'Mihai Para'
 
 def greet_catalin_ciprian_pasare():
 	return 'missing'

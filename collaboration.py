087123163c82ee18179c47d19fa44d84dd30d361
George <geogeo1003@gmail.com>
2022-04-28 20:14:15 +0300

    Am adaugat numele

--- collaboration.py
+++ collaboration.py
@@ -8,7 +8,7 @@ def greet_petrea_andrei():
 	return 'missing'
 
 def greet_george_leonard_batca():
-	return 'missing'
+	return 'George Leonard'
 
 def greet_elena_catalina_bogdan():
 	return 'missing'

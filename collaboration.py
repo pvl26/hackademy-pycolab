2c018e6ac8d1bb37341fd26eb6611a252af3f4c7
meli2502 <94045536+meli2502@users.noreply.github.com>
2022-04-28 20:27:36 +0300

    Am adaugat numele meu (#111)
    * Am adaugat numele meu
    
    Signed-off-by: meli2502 <melaniamarian@yahoo.com>
    
    * Am adaugat numele

--- collaboration.py
+++ collaboration.py
@@ -50,7 +50,7 @@ def greet_stefanita_ionita():
 	return 'missing'
 
 def greet_melania_valentina_marian():
-	return 'missing'
+	return 'Melania Valentina Marian'
 
 def greet_claudiu_mogodeanu():
 	return 'missing'
